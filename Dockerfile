66de8345ad8d05eac297841277af6979a9332f23
dapkunayte <gladiatormahotina@yandex.ru>
2022-03-12 17:39:30 +0700

    Update

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,8 @@
 FROM debian
-RUN apt-get update
+RUN apt-get update && apt-get upgrade -y
 RUN apt install gcc gdb gcc-multilib nano -y
+RUN apt-get install python3 -y
 COPY lab22.s .
-RUN gcc -m32 lab22.s -o lab22
-CMD ./lab22
+COPY prog.py .
+RUN gcc -m32 lab22.s -o lab22 
+CMD ./lab22 && python3 prog.py
--- prog.py
+++ prog.py
@@ -1,23 +1,11 @@
-import dis
-
-def func():
-	a = ["words", "world", "wilde", "wide", "wade", "wild", "winq", "wane", "won", "worm"]
-	print(a)
-	for i in range(len(a)):
-    		number = ord(a[i][len(a[i])-1])
-    		if number%2==0:
-        		byte = bin(number)
-        #print(a[i][len(a[i])-1])
-        #print(byte)
-        #print(int(bool(int(byte[len(byte) - 1]))))
-        #print(int(not bool(int(byte[len(byte) - 1]))))
-        		byte_less = byte[:-1]
-        #print(byte_less)
-        		new_byte = str(str(byte_less) + str(int(not bool(int(byte[len(byte) - 1])))))
-        #print(new_byte)
-        		rep = str(chr(int(new_byte,2)))
-        #print(rep)
-        		a[i] = a[i].replace(a[i][len(a[i])-1],rep)
-        #print(a[i])
-	print(a)
-dis.dis(func)
+a = ["words", "world", "wilde", "wide", "wade", "wild", "winq", "wane", "won", "worm"]
+print(a)
+for i in range(len(a)):
+    	number = ord(a[i][len(a[i])-1])
+    	if number%2==0:
+        	byte = bin(number)
+        	byte_less = byte[:-1]
+        	new_byte = str(str(byte_less) + str(int(not bool(int(byte[len(byte) - 1])))))
+        	rep = str(chr(int(new_byte,2)))
+        	a[i] = a[i].replace(a[i][len(a[i])-1],rep)
+print(a)
